23134a1cc23313749430522ebc901b146ae34d01
Diego Tapia <jtapiav4@est.ups.edu.ec>
2022-06-29 17:40:12 -0500

    Initial commit

---
+++ src/main/java/ups/ejercicio_03_06/Ejercicio_03_06.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ups.ejercicio_03_06;
+
+/**
+ *
+ * @author Diego
+ */
+public class Ejercicio_03_06 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
